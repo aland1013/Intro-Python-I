e17d19c94feb875841e1caa915b34918874629e0
aland1013 <adam.land@gmail.com>
2020-09-03 22:05:19 -0400

    fixed comprehensions.py

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,18 +10,18 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for x in range(5):
-    y.append(x + 1)
+y = [x for x in range(1, 6)]
+# for x in range(1, 6):
+#     y.append(x)
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-for x in range(10):
-    y.append(x ** 3)
+y = [x ** 3 for x in range(10)]
+# for x in range(10):
+#     y.append(x ** 3)
 
 print(y)
 
@@ -30,9 +30,9 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-for str in a:
-    y.append(str.upper())
+y = [str.upper() for str in a]
+# for str in a:
+#     y.append(str.upper())
 
 print(y)
 
@@ -42,10 +42,10 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-for str in x:
-    num = int(str)
-    if num % 2 == 0:
-        y.append(num)
+y = [num for num in x if int(num) % 2 == 0]
+# for str in x:
+#     num = int(str)
+#     if num % 2 == 0:
+#         y.append(num)
 
 print(y)
